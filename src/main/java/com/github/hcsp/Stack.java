b5885c49abf644ff0e7e5fa5d585eaa24433e9ce
TheBe4st <46705923+TheBe4st@users.noreply.github.com>
2020-02-25 10:15:55 +0800

    bugfix (#7)
    bugfix (#7)

--- src/main/java/com/github/hcsp/Stack.java
+++ src/main/java/com/github/hcsp/Stack.java
@@ -21,7 +21,9 @@ public class Stack {
         if (size == 0) {
             throw new EmptyStackException();
         }
-        return elements[--size];
+        Object ret = elements[size - 1];
+        elements[--size] = null;
+        return ret;
     }
 
     /**
